113c581fcb0390c8d65f8d15d6695c2370b5e27b
amantripathi01 <18uec052@lnmiit.ac.in>
2025-07-07 19:21:29 +0530

    Add API Tests (Karate Framework, Claude AI) generated by RoostGPT
    Using AI Model claude-sonnet-4-20250514

---
+++ src/test/java/org/springframework/api_tests/SlackapiSlackApiSpecsRefsHeadsMasterWebApiSlackWebOpenapiV2/SlackapiSlackApiSpecsRefsHeadsMasterWebApiSlackWebOpenapiV2Test.java
@@ -0,0 +1,27 @@
+
+package org.springframework.api_tests.SlackapiSlackApiSpecsRefsHeadsMasterWebApiSlackWebOpenapiV2;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+// import com.intuit.karate.http.HttpServer;
+// import com.intuit.karate.http.ServerConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SlackapiSlackApiSpecsRefsHeadsMasterWebApiSlackWebOpenapiV2Test {
+
+	@Test
+	void testAll() {
+		String urlbase = System.getenv().get("URL_BASE");
+		String authtoken = System.getenv().get("AUTH_TOKEN");
+		Results results = Runner.path(
+				"src/test/java/org/springframework/api_tests/SlackapiSlackApiSpecsRefsHeadsMasterWebApiSlackWebOpenapiV2")
+			.systemProperty("URL_BASE", urlbase)
+			.systemProperty("AUTH_TOKEN", authtoken)
+			.reportDir("testReport")
+			.parallel(1);
+		assertEquals(0, results.getFailCount(), results.getErrorMessages());
+	}
+
+}
